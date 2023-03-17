3e70c3933c4db37de82a31394151e3e1ac90785b
jzwar <zwar@ilsb.tuwien.ac.at>
2023-03-17 12:38:11 +0100

    Working derivatives of the bilinear form
    Function derivative works

--- examples/linear_elasticity_expressions.cpp
+++ examples/linear_elasticity_expressions.cpp
@@ -10,10 +10,95 @@
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-//! [Include namespace]
 #include <gismo.h>
 
 using namespace gismo;
+
+namespace gismo::expr {
+
+/*
+Expression for the Frobenius matrix product between a matrix of matrices and a
+single matrix with cardinality 1
+*/
+template <typename E1, typename E2>
+class frobenius_prod_expr : public _expr<frobenius_prod_expr<E1, E2>> {
+ public:
+  typedef typename E1::Scalar Scalar;
+  enum {
+    ScalarValued = 0,
+    Space = E1::Space,
+    ColBlocks = 0  // E1::ColBlocks || E2::ColBlocks
+  };
+
+ private:
+  typename E1::Nested_t _u;
+  typename E2::Nested_t _v;
+
+  mutable gsMatrix<Scalar> res;
+
+ public:
+  frobenius_prod_expr(_expr<E1> const& u, _expr<E2> const& v) : _u(u), _v(v) {
+    // todo: add check() functions, which will evaluate expressions on an empty
+    // matrix (no points) to setup initial dimensions ???
+    GISMO_ASSERT(_u.rows() == _v.rows(), "Wrong dimensions "
+                                             << _u.rows() << "!=" << _v.rows()
+                                             << " in % operation");
+    GISMO_ASSERT(_u.cols() == _v.cols(), "Wrong dimensions "
+                                             << _u.cols() << "!=" << _v.cols()
+                                             << " in %operation");
+  }
+
+  const gsMatrix<Scalar>& eval(const index_t k) const {
+    // Evaluate Expressions and cardinality
+    const index_t u_r = _u.rows();
+    const index_t u_c = _u.cols();
+
+    // Cardinality impl refers to the cols im a matrix
+    auto A = _u.eval(k);
+    auto B = _v.eval(k);
+    const index_t A_rows = A.rows() / u_r;
+    const index_t A_cols = A.cols() / u_c;
+    GISMO_ASSERT(
+        _v.cardinality() == 1,
+        "Expression is only for second expressions with cardinality 1");
+    GISMO_ASSERT((u_r == _v.cols()) && (u_c == _v.cols()),
+                 "Both expressions need to be same size");
+    GISMO_ASSERT(B.size() == _v.rows() * _v.cols(),
+                 "RHS expression contains more than one matrix");
+    res.resize(A_rows, A_cols);
+    for (index_t i = 0; i < A_rows; ++i)
+      for (index_t j = 0; j < A_cols; ++j)
+        res(i, j) =
+            (A.block(i * u_r, j * u_c, u_r, u_c).array() * B.array()).sum();
+    return res;
+  }
+
+  index_t rows() const { return 1; }
+  index_t cols() const { return 1; }
+
+  void parse(gsExprHelper<Scalar>& evList) const {
+    _u.parse(evList);
+    _v.parse(evList);
+  }
+
+  const gsFeSpace<Scalar>& rowVar() const { return _u.rowVar(); }
+  const gsFeSpace<Scalar>& colVar() const { return _u.colVar(); }  // overwrite
+
+  void print(std::ostream& os) const {
+    os << "(";
+    _u.print(os);
+    os << " % ";
+    _v.print(os);
+    os << ")";
+  }
+};
+template <typename E1, typename E2>
+EIGEN_STRONG_INLINE frobenius_prod_expr<E1, E2> const frobenius(
+    _expr<E1> const& u, _expr<E2> const& v) {
+  return frobenius_prod_expr<E1, E2>(u, v);
+}
+
+}  // namespace gismo::expr
 
 // Global Typedefs
 typedef gsExprAssembler<>::geometryMap geometryMap;
@@ -27,29 +112,49 @@ typedef gsExprAssembler<>::solution solution;
  *
  * to be deleted
  */
-auto ComputeSensitivityFD(const gsMultiPatch<>& mp_dx, const gsOptionList& Aopt,
-                          const gsMultiBasis<>& function_basis,
-                          const double& lame_lambda, const double& lame_mu,
-                          const gsMatrix<>& solVector_reference,
-                          const gsBoundaryConditions<>& bc) {
+auto ComputeSensitivityFD(const std::string& fn, const double& lame_lambda,
+                          const double& lame_mu, const double& rho,
+                          const int ref,
+                          const gsMatrix<>& solVector_reference) {
+  // Assuming defaults
+  int mp_id{0}, source_id{1}, bc_id{2}, ass_opt_id{3};
+
+  gsFileData<> fddx("dx." + fn);
+  gsInfo << "Loaded file " << fddx.lastPath() << "\n";
+  gsMultiPatch<> mpdx;
+  fddx.getId(mp_id, mpdx);
+  gsBoundaryConditions<> bcdx;
+  fddx.getId(bc_id, bcdx);
+  bcdx.setGeoMap(mpdx);
   gsExprAssembler<> expr_assembler(1, 1);
+  gsOptionList Aopt;
+  fddx.getId(ass_opt_id, Aopt);
   expr_assembler.setOptions(Aopt);
 
   // Elements used for numerical integration
+  gsMultiBasis<> function_basis(
+      mpdx, true);  // true: poly-splines (not NURBS)// h-refine each basis
+  for (int r = 0; r < ref; ++r) {
+    function_basis.uniformRefine();
+  }
   expr_assembler.setIntegrationElements(function_basis);
 
   // Set the geometry map
-  geometryMap geom_expr = expr_assembler.getMap(mp_dx);
+  geometryMap geom_expr = expr_assembler.getMap(mpdx);
 
   // Set the discretization space
   space u_trial = expr_assembler.getSpace(function_basis, 2);
 
   // Solution space
+  gsFunctionExpr<> f;
+  fddx.getId(1, f);
+  gsInfo << "Source function " << f << "\n";
+  auto ff = expr_assembler.getCoeff(f, geom_expr);
 
   gsMatrix<> solVector{solVector_reference};
   solution solution_expression = expr_assembler.getSolution(u_trial, solVector);
 
-  u_trial.setup(bc, dirichlet::l2Projection, 0);
+  u_trial.setup(bcdx, dirichlet::l2Projection, 0);
   // Compute the system matrix and right-hand side
 
   // Assemble
@@ -58,22 +163,29 @@ auto ComputeSensitivityFD(const gsMultiPatch<>& mp_dx, const gsOptionList& Aopt,
   auto BL_lambda_1 = idiv(solution_expression, geom_expr).val();  // validated
   auto BL_lambda_2 = idiv(u_trial, geom_expr);                    // validated
   auto BL_lambda =
-      lame_lambda * BL_lambda_2 * BL_lambda_1 * meas_expr;    // validated
-  auto BL_mu1_1 = ijac(solution_expression, geom_expr);       // validated
-  auto BL_mu1_2 = ijac(u_trial, geom_expr);                   // validated
-  auto BL_mu1 = lame_mu * (BL_mu1_2 % BL_mu1_1) * meas_expr;  // validated
+      lame_lambda * BL_lambda_2 * BL_lambda_1 * meas_expr;         // validated
+  auto BL_mu1_1 = ijac(solution_expression, geom_expr);            // validated
+  auto BL_mu1_2 = ijac(u_trial, geom_expr);                        // validated
+  auto BL_mu1 = lame_mu * (BL_mu1_2 % BL_mu1_1) * meas_expr;       // validated
+  auto BL_mu2_1 = ijac(solution_expression, geom_expr).cwisetr();  // validated
+  auto& BL_mu2_2 = BL_mu1_2;                                       // validated
+  auto BL_mu2 = lame_mu * (BL_mu2_2 % BL_mu2_1) * meas_expr;       // validated
+  auto LF_1 = -rho * u_trial * ff * meas_expr;                     // validated
   expr_assembler.initSystem();
   expr_assembler.clearRhs();
   expr_assembler.assemble(BL_lambda);
+  expr_assembler.assemble(BL_mu1);
+  expr_assembler.assemble(BL_mu2);
+  expr_assembler.assemble(LF_1);
 
   // Evaluator for simplified expressions
   gsExprEvaluator<> expression_evaluator(expr_assembler);
   gsMatrix<> evalPoint(2, 1);
   evalPoint << .25, .6;
 
-  gsInfo << "BL_mu1_2 : \n"
-         << std::setprecision(20)
-         << expression_evaluator.eval(BL_mu1_2, evalPoint) << std::endl;
+  // gsInfo << "BL_mu1_2 : \n"
+  //        << std::setprecision(20)
+  //        << expression_evaluator.eval(BL_mu1_2, evalPoint) << std::endl;
 
   // Return the matrix to evaluate the residual
   return expr_assembler.rhs();
@@ -231,7 +343,8 @@ int main(int argc, char* argv[]) {
   expr_assembler.initSystem();
   setup_time += timer.stop();
 
-  gsInfo << expr_assembler.numDofs() << std::endl;
+  gsInfo << "Number of degrees of freedom : " << expr_assembler.numDofs()
+         << std::endl;
 
   timer.restart();
 
@@ -248,9 +361,7 @@ int main(int argc, char* argv[]) {
   auto bilin_combined = (bilin_lambda + bilin_mu_1 + bilin_mu_2);
 
   expr_assembler.assemble(bilin_combined);
-  gsInfo << "Combined bilinear form is assembled" << std::endl;
   expr_assembler.assemble(lin_form);
-  gsInfo << "Volume part of linear form is assembled" << std::endl;
 
   // Compute the Neumann terms defined on physical space
   // auto g_N = expr_assembler.getBdrFunction(geom_expr);
@@ -334,6 +445,7 @@ int main(int argc, char* argv[]) {
     gsInfo << "It has \t" << expression.rows() << " rows and \t"
            << expression.cols() << " cols" << std::endl;
     gsInfo << "Is Vector : " << expression.isVector()
+           << " is Vector^T : " << expression.isVectorTr()
            << " is Matrix : " << expression.isMatrix() << std::endl;
     gsInfo << "The cardinality of the expression is : "
            << expression.cardinality() << std::endl;
@@ -368,13 +480,15 @@ int main(int argc, char* argv[]) {
       ////////////////////////////////
       // Derivative of the LHS Form //
       ////////////////////////////////
+      expr_assembler.clearRhs();
+      expr_assembler.clearMatrix();
 
       // Auxiliary expressions
       auto jacobian = jac(geom_expr);                      // validated
       auto inv_jacs = jacobian.ginv();                     // validated
+      auto meas_expr = meas(geom_expr);                    // validated
       auto djacdc = jac(u_trial);                          // validated
       auto aux_expr = (djacdc * inv_jacs).tr();            // validated
-      auto meas_expr = meas(geom_expr);                    // validated
       auto meas_expr_dx = meas_expr * (aux_expr).trace();  // validated
 
       // Start to assemble the bilinear form with the known solution field
@@ -385,135 +499,96 @@ int main(int argc, char* argv[]) {
       auto BL_lambda_2 = idiv(u_trial, geom_expr);     // validated
       auto BL_lambda =
           lame_lambda * BL_lambda_2 * BL_lambda_1 * meas_expr;  // validated
+
       // trace(A * B) = A:B^T
-      auto BL_lambda_1_dx =
-          (ijac(solution_expression, geom_expr) * aux_expr.tr())
-              .trace()
-              .tr();                                                // validated
+      auto BL_lambda_1_dx = frobenius(
+          aux_expr, ijac(solution_expression, geom_expr));          // validated
       auto BL_lambda_2_dx = (ijac(u_trial, geom_expr) % aux_expr);  // validated
+
       auto BL_lambda_dx =
           lame_lambda * BL_lambda_2 * BL_lambda_1 * meas_expr_dx -
           lame_lambda * BL_lambda_2_dx * BL_lambda_1 * meas_expr -
           lame_lambda * BL_lambda_2 * BL_lambda_1_dx * meas_expr;  // validated
 
+      // Assemble
+      expr_assembler.assemble(BL_lambda_dx);
+
       // 2. Bilinear form of mu (first part)
+      // BL_mu1_2 seems to be in a weird order with [jac0, jac2] leading
+      // to [2x(2nctps)]
       auto BL_mu1_1 = ijac(solution_expression, geom_expr);       // validated
       auto BL_mu1_2 = ijac(u_trial, geom_expr);                   // validated
       auto BL_mu1 = lame_mu * (BL_mu1_2 % BL_mu1_1) * meas_expr;  // validated
-      auto BL_mu1_1_dx = (ijac(solution_expression, geom_expr) *
-                          aux_expr.tr());  //          validated
 
-      auto BL_mu1_2_dx = ijac(u_trial, geom_expr).cwisetr() * aux_expr;  // WIP
-      // auto BL_mu1_dx = -lame_mu * (BL_mu1_2 % BL_mu1_1_dx) * meas_expr -
-      //                  lame_mu * (BL_mu1_2_dx % BL_mu1_1) * meas_expr +
-      //                  lame_mu * (BL_mu1_2 % BL_mu1_1) * meas_expr_dx;
+      auto BL_mu1_1_dx = -(ijac(solution_expression, geom_expr) *
+                           aux_expr.cwisetr());  //          validated
+      auto BL_mu1_2_dx =
+          -(jac(u_trial) * inv_jacs * aux_expr.cwisetr());  // validated
 
-      gsInfo << "djacdc : \n"
-             << std::setprecision(20)
-             << expression_evaluator.eval(djacdc, evalPoint) << std::endl;
-
-      gsInfo << "djacdc * inv_jacs : \n"
-             << std::setprecision(20)
-             << expression_evaluator.eval(djacdc * inv_jacs, evalPoint)
-             << std::endl;
-
-      gsInfo << "aux_expr : \n"
-             << std::setprecision(20)
-             << expression_evaluator.eval(aux_expr, evalPoint) << std::endl;
-
-      // print_function_expressions("BL_mu1", BL_mu1);
-      // print_function_expressions("BL_mu1_1", BL_mu1_1);
-      // print_function_expressions("BL_mu1_1_dx", BL_mu1_1_dx);
-      // print_function_expressions("BL_mu1_2", BL_mu1_2);
-      // print_function_expressions("BL_mu1_2_dx", BL_mu1_2_dx);
-      // print_function_expressions(
-      //     "-lame_mu * (BL_mu1_2 % BL_mu1_1_dx) * meas_expr",
-      //     -lame_mu * (BL_mu1_2 % BL_mu1_1_dx) * meas_expr);
-      // print_function_expressions(
-      //     "lame_mu * (BL_mu1_1 % BL_mu1_2).tr()",
-      //     lame_mu * (BL_mu1_2 % BL_mu1_1) * meas_expr_dx);
+      auto BL_mu1_dx0 =
+          lame_mu * BL_mu1_2 % BL_mu1_1_dx * meas_expr;  // validated
+      auto BL_mu1_dx1 =
+          lame_mu * frobenius(BL_mu1_2_dx, BL_mu1_1) * meas_expr;  // validated
+      auto BL_mu1_dx2 = lame_mu * frobenius(BL_mu1_2, BL_mu1_1).cwisetr() *
+                        meas_expr_dx;  // validated
 
       // Assemble
-      expr_assembler.clearRhs();
-      expr_assembler.assemble(BL_mu1);
-      expr_assembler.clearMatrix();
-      expr_assembler.assemble(bilin_mu_1);
-      // gsMatrix<> matrix = expr_assembler.matrix();
-      // gsInfo << "First part of the matrix computed : \n" << matrix <<
-      // std::endl;
+      expr_assembler.assemble(BL_mu1_dx0);
+      expr_assembler.assemble(BL_mu1_dx1);
+      expr_assembler.assemble(BL_mu1_dx2);
+
+      // 2. Bilinear form of mu (first part)
+      auto BL_mu2_1 =
+          ijac(solution_expression, geom_expr).cwisetr();         // validated
+      auto& BL_mu2_2 = BL_mu1_2;                                  // validated
+      auto BL_mu2 = lame_mu * (BL_mu2_2 % BL_mu2_1) * meas_expr;  // validated
+
+      auto inv_jac_T = inv_jacs.tr();
+      auto BL_mu2_1_dx = -inv_jac_T * jac(u_trial).tr() * inv_jac_T *
+                         jac(solution_expression).cwisetr();  // validated
+      auto& BL_mu2_2_dx = BL_mu1_2_dx;                        // validated
+
+      auto BL_mu2_dx0 =
+          lame_mu * BL_mu2_2 % BL_mu2_1_dx * meas_expr;  // validated
+      auto BL_mu2_dx1 =
+          lame_mu * frobenius(BL_mu2_2_dx, BL_mu2_1) * meas_expr;  // validated
+      auto BL_mu2_dx2 = lame_mu * frobenius(BL_mu2_2, BL_mu2_1).cwisetr() *
+                        meas_expr_dx;  // validated
+
+      // Assemble
+      expr_assembler.assemble(BL_mu2_dx0);
+      expr_assembler.assemble(BL_mu2_dx1);
+      expr_assembler.assemble(BL_mu2_dx2);
+
+      // Linear Form Part
+      auto LF_1 = -rho * u_trial * ff * meas_expr;
+      auto LF_1_dx = -rho * u_trial * ff * meas_expr_dx;
+
+      // Assemble
+      expr_assembler.assemble(LF_1_dx);
 
       /////////////////////////////////////////
       // This section is meant for DEBUGGING //
       /////////////////////////////////////////
 
       if (fd_test) {
-        expr_assembler.clearRhs();
+        expr_assembler.assemble(LF_1);
+        expr_assembler.assemble(BL_mu2);
+        expr_assembler.assemble(BL_mu1);
         expr_assembler.assemble(BL_lambda);
-        const auto lame_lambda_rhs = expr_assembler.rhs();
-        gsFileData<> fddx("dx." + fn);
-        gsInfo << "Loaded file " << fddx.lastPath() << "\n";
-        gsMultiPatch<> mpdx;
-        fddx.getId(mp_id, mpdx);
-        gsBoundaryConditions<> bcdx;
-        fd.getId(bc_id, bcdx);
-        bc.setGeoMap(mpdx);
-        auto rhs_of_fd_system = ComputeSensitivityFD(
-            mpdx, Aopt, function_basis, lame_lambda, lame_mu, solVector, bc);
+        // Assemble
+        gsMatrix<> matrix = expr_assembler.matrix();
+        gsInfo << "First part of the matrix computed : \n"
+               << matrix << std::endl;
+        const auto rhs_orig = expr_assembler.rhs();
+        auto rhs_of_fd_system = ComputeSensitivityFD(fn, lame_lambda, lame_mu,
+                                                     rho, numRefine, solVector);
 
-        // gsInfo << "FD Approximation for lambda part of matrix assembly is
-        // :\n"
-        //        << (rhs_of_fd_system - lame_lambda_rhs) / ddx << std::endl;
+        gsInfo << "FD Approximation for lambda part of matrix assembly is:\n"
+               << (rhs_of_fd_system - rhs_orig) / ddx << std::endl;
       }
-
-      // 2. Bilinear form of mu (first part)
-      auto BL_mu2_1 = ijac(solution_expression, geom_expr).cwisetr();
-      auto BL_mu2_2 = ijac(u_trial, geom_expr);
-      auto BL_mu2 = lame_mu * (BL_mu2_2 % BL_mu2_1) * meas_expr;
-
-      // Linear Form Part
-      auto LF_1 = -rho * u_trial * ff * meas_expr;
-
-      // Assemble
     }
   }
-
   return EXIT_SUCCESS;
-
-  //! [Here starts the work in progress ]
-  ///////////////////////////////////////
-  // Differentiating the linear system //
-  ///////////////////////////////////////
-  bool evaluate_expr_v{false};
-  if (evaluate_expr_v) {
-    // For local evaluation prior to testing
-    gsExprEvaluator<> evaluator{expr_assembler};
-    gsMatrix<> evalPoint(2, 1);
-    evalPoint << .5, .5;
-
-    // Modified / simplified expressions:
-    auto d_jac_d_c = jac(u_trial);  // assumes isoparametrics
-    auto inv_jac = jac(geom_expr).inv();
-    auto d_jac_inv_d_c = -inv_jac * d_jac_d_c;
-
-    // Differntiating lambda
-    auto bilin_lambda_der1 =
-        (d_jac_inv_d_c * ijac(solution_expression, geom_expr)).trace() *
-        idiv(u_trial, geom_expr).tr();
-    // auto bilin_lambda_der2 =
-    //     idiv(solution_expression, geom_expr) * (d_jac_inv_d_c *
-    //     igrad(u_trial, geom_expr).tr());
-    auto bilin_lambda_der = lame_lambda * (bilin_lambda_der1)*meas(geom_expr);
-
-    auto derivative = bilin_lambda_der;
-
-    gsInfo
-        << "\nFirst Expression:\n"
-        << evaluator.eval(
-               (d_jac_inv_d_c * ijac(solution_expression, geom_expr)).trace() *
-                   idiv(u_trial, geom_expr).tr(),
-               evalPoint)
-        << "\n";
-
-    return EXIT_SUCCESS;
-  }
+}
 }  // end main
